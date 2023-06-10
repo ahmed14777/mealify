3eea7099932d7fbd506e79352afa821824a8eff6
ahmed14777 <ah9621777@gmail.com>
2023-06-10 18:19:03 +0200

    add gallery&contact

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ const navLinks = document.querySelectorAll("links .link");
 const checkBox = document.querySelector("input[type='checkbox']");
 const home = document.querySelector("#home");
 const chefs = document.querySelector("#Chefs");
+const gallery = document.querySelector("#Gallery");
+const contact = document.querySelector("#Contact");
+const chefsCardText = document.querySelector("#chefs .cards .card .text");
 
 //  add class to animate the nav bar in the large screen
 function linksIn() {
@@ -14,4 +17,12 @@ function linksIn() {
 checkBox.addEventListener("change", function () {
     home.classList.toggle("dark-mode");
     chefs.classList.toggle("dark-mode");
+    gallery.classList.toggle("dark-mode");
+    contact.classList.toggle("dark-mode");
+    console.log(home.classList.contains("dark-mode"));
+    if (home.classList.contains("dark-mode")) {
+        console.log("yes");
+    } else {
+        console.log("no");
+    }
 });
